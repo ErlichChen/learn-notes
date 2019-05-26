83b6938680e7b559fcc79955f37cf33dcdf405f5
erlichchen <chenxuewei.dev@gmail.com>
2019-05-26 16:16:34 +0800

    inline asm in c

--- c&cpp/tutorial/1-inline-asm.c
+++ c&cpp/tutorial/1-inline-asm.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+int gcd(int var1, int var2)
+{
+	int result = 0;
+	__asm__ __volatile__ (
+		"movl %1, %%eax;"
+		"movl %2, %%ebx;"
+		"CONTD: cmpl $0, %%ebx;"
+		"je DONE;"
+		"xorl %%edx, %%edx;"
+		"idivl %%ebx;"
+		"movl %%ebx, %%eax;"
+		"movl %%edx, %%ebx;"
+		"jmp CONTD;"
+		"DONE: movl %%eax, %0;"
+		:"=r"(result)
+		:"r"(var1), "r"(var2)
+	);
+	return result;
+}
+
 int main(void)
 {
-	printf("Hello world\n");
+	int result = 0;
+  int input = 1;
+
+	asm volatile (
+	    "movl %1, %0\n"
+	    : "=r"(result)
+	    : "r"(input)
+	    );
+
+	printf("result = %d\n", result);
+	printf("input = %d\n", input);
 	int a;
 	int b;
 	int c;
@@ -20,5 +50,9 @@ int main(void)
 	printf("a = %d\n", a);
 	printf("b = %d\n", b);
 
+	int first = 15, second = 35;
+	result = gcd(first, second);
+	printf("result = %d\n", result);
 	return 0;
 }
+
