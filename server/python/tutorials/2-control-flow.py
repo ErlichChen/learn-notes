a575f33f2130b8b453301689aa543822cd56c71f
erlichchen <chenxuewei.dev@gmail.com>
2020-04-10 16:58:37 +0800

    python control flow

--- server/python/tutorials/2-control-flow.py
+++ server/python/tutorials/2-control-flow.py
@@ -3,9 +3,11 @@ control flow
 '''
 
 num = input("Please input a number: ")
+print(type(num))
+print(type(int(num)))
 if int(num) < 0:
 	print("The number is less than zero.")
-elif num == 0:
+elif int(num) == 0:
 	print("The number is equal zero.")
 else:
 	print("The number is greater than zero.")
